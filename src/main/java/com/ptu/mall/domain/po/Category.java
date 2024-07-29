48fcf16b98f51d9b3b894cede39403d7ec9a2ed4
Wu Jing Jie <KZBN_Wu@outlook.com>
2024-07-29 16:46:48 +0800

    添加了注解

--- src/main/java/com/ptu/mall/domain/po/Category.java
+++ src/main/java/com/ptu/mall/domain/po/Category.java
@@ -6,10 +6,7 @@ import java.time.LocalDateTime;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 /**
@@ -23,6 +20,7 @@ import lombok.experimental.Accessors;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("category")
